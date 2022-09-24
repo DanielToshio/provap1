b3ef6c0a99589eef81fbc841df1d84a192b41dbf
DanielToshio <802.261@alunos.unigran.br>
2022-09-23 21:14:57 -0300

    atuliza tela

--- app/src/main/java/br/unigran/provap1/MainActivity.java
+++ app/src/main/java/br/unigran/provap1/MainActivity.java
@@ -1,11 +1,19 @@
 package br.unigran.provap1;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,7 +45,51 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter adapter = new ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,dados);
         listagem.setAdapter(adapter);
+        acoes();
 
+    }
+
+    private void acoes() {
+        listagem.setOnItemLongClickListener(
+                new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapterView,
+                                                   View view, int i, long l) {
+                        new AlertDialog.Builder(view.getContext())
+                                .setMessage("Deseja realmente remover")
+                                .setPositiveButton("Confirmar",
+                                        new DialogInterface.OnClickListener() {
+                                            @Override
+                                            public void onClick(DialogInterface dialogInterface,
+                                                                int i) {
+                                                
+                                            }
+                                        })
+                                .setNegativeButton("cancelar",null)
+                                .create().show();
+                        return false;
+                    }
+                });
+
+    }
+
+    public void salvar(View view){
+        Abastecimento abastecimento = new Abastecimento();
+        abastecimento.setKm(km.getText().toString());
+        abastecimento.setDiaabastecido(diaabastecido.getText().toString());
+        abastecimento.setQtdabastecida(qtdabastecida.getText().toString());
+        abastecimento.setValor(valor.getText().toString());
+
+
+        Snackbar.make(this,view,"ertrt", BaseTransientBottomBar.LENGTH_SHORT).
+//                setAction(R.string.app_name, new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//
+//                    }
+//                }).
+        show();
+        Toast.makeText(this,"Salvo com sucesso",Toast.LENGTH_SHORT).show();
     }
 }
 
